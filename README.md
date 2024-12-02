55f52d8757e8a5b586e9ad333b7b086f1ce5ebdd
Jonathan Chen <jonafish10@gmail.com>
2024-12-01 19:57:46 -0500

    Finish day 1 part 2

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ My solutions to the 2024 [Advent of Code](https://adventofcode.com/2024), in Rac
 
 This is my first time using Racket, so my solutions may not be the most idiomatic. It is exciting to try, though. (Sometimes I ask Chat GPT for idiomatic syntax pointers; it is not very good at this but it gives me a starting point to look further.)
 
-| Day                       | Part 1                                                           | Part 2 |
-| ------------------------- | ---------------------------------------------------------------- | ------ |
-| [Day 1](./day_1/soln.rkt) | Construct the two separate lists and apply difference, then add. |        |
+| Day                       | Part 1                               | Part 2                                                                         |
+| ------------------------- | ------------------------------------ | ------------------------------------------------------------------------------ |
+| [Day 1](./day_1/soln.rkt) | Apply difference pairwise, then add. | Map each element in first list to its similarity score and add. (not memoized) |
--- day_1/soln.rkt
+++ day_1/soln.rkt
@@ -3,37 +3,52 @@
 #;(provide solve-a solve-b)
 (provide 1d-distance)
 
-;;; PART A
+;;; PART 1
 (define/contract (solve-a filename)
   (-> string? exact-integer?)
-  (with-input-from-file filename
-    (lambda ()
-      (define-values (AS BS) (read-loclists)) ; unsorted, in reverse order
-      (define AS-SORTED (sort AS <))
-      (define BS-SORTED (sort BS <))
+  
+  (define-values (AS BS) (read-loclists filename)) ; unsorted, in reverse order
+  (define AS-SORTED (sort AS <))
+  (define BS-SORTED (sort BS <))
 
-      ; multiple complex inputs :DDDD
-      (foldr + 0 (map 1d-distance AS-SORTED BS-SORTED)))))
+  ; multiple complex inputs :DDDD
+  (foldr + 0 (map 1d-distance AS-SORTED BS-SORTED)))
 
 ; read lines of "a b" and produces lists as and bs
-(define/contract (read-loclists)
-  (-> (values (listof exact-integer?) (listof exact-integer?)))
-  (for/fold ([as '()]
-             [bs '()])
-            ([line (in-lines)])
-    (define line-parts (regexp-split #px"\\s+" line))
-    (values (cons (string->number (first line-parts)) as) 
-            (cons (string->number (second line-parts)) bs))))
+(define/contract (read-loclists filename)
+  (-> string? (values (listof exact-integer?) (listof exact-integer?)))
+  (with-input-from-file filename
+    (lambda ()
+      (for/fold ([as '()]
+                 [bs '()])
+                ([line (in-lines)])
+        (define line-parts (regexp-split #px"\\s+" line))
+        (values (cons (string->number (first line-parts)) as) 
+                (cons (string->number (second line-parts)) bs))))))
 
 ; Euclidean distance (absolute value of difference)
 (define/contract (1d-distance a b)
   (-> exact-integer? exact-integer? (and/c exact-integer? (not/c negative?)))
   (abs (- a b)))
 
+
+;;; PART 2
+(define/contract (solve-b filename)
+  (-> string? exact-integer?)
+  
+  (define-values (AS BS) (read-loclists filename)) ; unsorted, in reverse order
+
+  (foldr + 0
+         (map (lambda (a)
+                (* a
+                   (count (lambda (b) (= a b)) BS)))
+              AS)))
+
+
 (module+ test
   (require rackunit)
   (check-equal? (solve-a "input-a-test.txt") 11)
-  #;(check-equal? (solve-b "input-a-test.txt") 31)
+  (check-equal? (solve-b "input-a-test.txt") 31)
 
 
   ; 1d-distance
@@ -45,4 +60,7 @@
   (check-equal? (1d-distance 1 5) 4)
   (check-equal? (1d-distance 1 -5) 6))
 
+
+
 #;(solve-a "input-a.txt")
+#;(solve-b "input-a.txt")
